8a3d90fd819e76ab3f8c8f6e403dd83601da6d1b
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-03 23:49:39 +0500

    Created the database and connected that database with entity frameWork with the help of connection strings and basic configuration

---
+++ PracticeProject_DOTNET/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticeProject_DOTNET.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } //primary key for the table
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
+ 
