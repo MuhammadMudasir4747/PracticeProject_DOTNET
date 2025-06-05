ca2e6be14f5443719dc68282f54ff656848b1340
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-05 23:00:36 +0500

    Retreived the categories from category list from database and rendered on UI through view

--- PracticeProject_DOTNET/Controllers/CategoryController.cs
+++ PracticeProject_DOTNET/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PracticeProject_DOTNET.Data;
+using PracticeProject_DOTNET.Models;
 
 namespace PracticeProject_DOTNET.Controllers
 {
     public class CategoryController : Controller
     {
+
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
