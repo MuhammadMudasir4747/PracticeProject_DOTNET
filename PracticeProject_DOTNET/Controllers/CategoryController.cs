9fd0f9bf189874ab1d4cfe60c299f33f7615442a
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-12 19:50:53 +0500

    Added Edit Category functionality for categories

--- PracticeProject_DOTNET/Controllers/CategoryController.cs
+++ PracticeProject_DOTNET/Controllers/CategoryController.cs
@@ -32,5 +32,38 @@ namespace PracticeProject_DOTNET.Controllers
             }
             return RedirectToAction("Index", "Category");
         }
+
+       
+        //Edit --- Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CategoryfromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if(CategoryfromDb == null) {
+                return NotFound();    
+            }
+
+
+            return View(CategoryfromDb);
+        }
+
+        // EDIT - POST ✅ YOU NEEDED THIS PART
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(category); // show validation messages
+        }
     }
 }
