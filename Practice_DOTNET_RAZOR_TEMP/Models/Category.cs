c2f1ca5b1b31859bc42379ca999f341df430ddd7
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-13 21:44:52 +0500

    Created the Model in the Razor pages project

---
+++ Practice_DOTNET_RAZOR_TEMP/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice_DOTNET_RAZOR_TEMP.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public String Name { get; set; }
+
+        [DisplayName("Display Ordeer")]
+        [Range(1, 100, ErrorMessage ="Display Order must be between 1 and 100")]
+        public int DisplayOrder{ get; set; }
+    }
+}
