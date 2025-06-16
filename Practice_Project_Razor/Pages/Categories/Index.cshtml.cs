10cad8484c8636b9d35e256001a7442226422840
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-16 19:33:48 +0500

    Retreived the Categories from Database using Razor page

--- Practice_Project_Razor/Pages/Categories/Index.cshtml.cs
+++ Practice_Project_Razor/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Practice_Project_Razor.Data;
+using Practice_Project_Razor.Models;
 
 namespace Practice_Project_Razor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db =  db;
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToArray().ToList();
+
         }
     }
 }
