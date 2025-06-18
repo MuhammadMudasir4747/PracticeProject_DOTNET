d4e2b1f6819127adaeacc2da312fa92fc26ad806
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-19 00:55:44 +0500

    Working with repositories

--- PracticeProject.DataAccess/Repository/IRepository/IRepository.cs
+++ PracticeProject.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,17 @@ namespace PracticeProject.DataAccess.Repository.IRepository
 {
     internal interface IRepository<T> where T : class
     {
+        //T will be category or any other generic model on
+        // which we want to perform operations or rather we want
+        //to interact with DbContext
+        
+        IEnumerable<T> GetAll(); //retreiving all catgs
+        T Get(Expression<Func<T, bool>> filter); //Get individual
+        void Add(T entity);
+        void Update(T entity);  
+        void Delete(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
 
     }
 }
