637f581814301b84955c4010f23bd07613c312a3
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-19 19:34:53 +0500

    Created the interface and Class Repository for the Category

---
+++ PracticeProject.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using PracticeProject.DataAccess.Repository.IRepository;
+using PracticeProject.Models;
+using PracticeProject_DOTNET.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRespository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base (db) 
+        {
+            _db = db;   
+        }
+        public void Save()
+        {
+          _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+           _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ PracticeProject.DataAccess/Repository/IRepository/ICategoryRespository.cs
@@ -0,0 +1,15 @@
+﻿using PracticeProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRespository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
