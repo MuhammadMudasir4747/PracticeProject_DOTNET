30a31d58a62c15a5abb0d1d4faf6fee6d64c02d7
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-06-21 17:15:49 +0500

    Created an Interface and Class for UnitOfWork Concept

--- PracticeProject.DataAccess/Repository/UnitOfWork.cs
+++ PracticeProject.DataAccess/Repository/UnitOfWork.cs
@@ -4,26 +4,27 @@ using PracticeProject_DOTNET.DataAccess.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PracticeProject.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRespository
+    public class UnitOfWork : IUnitOfWork
     {
+
         private ApplicationDbContext _db;
-        public CategoryRepository(ApplicationDbContext db) : base (db) 
+        public ICategoryRespository Category { get; private set; }
+        public UnitOfWork(ApplicationDbContext db) 
         {
-            _db = db;   
-        }
-      
-
-        public void Update(Category obj)
-        {
-           _db.Categories.Update(obj);
+            _db = db;
+            Category = new CategoryRepository(_db);
         }
 
-    
+        
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
